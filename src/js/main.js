2ad7089d93f6e7506ea0fa26bc21a912ec2fd887
feilding <feilding.cage@gmail.com>
2015-03-04 14:38:17 +0000

    integration w spreadsheets

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,16 @@
 define([
+    'get',
     'rvc!templates/appTemplate',
     'rvc!templates/navTemplate',
     'rvc!templates/podcastTemplate',
+    'rvc!templates/playerTemplate',
     'iframe-messenger'
 ], function(
+    get,
     AppTemplate,
     NavTemplate,
     PodcastTemplate,
+    PlayerTemplate,
     iframeMessenger
 ) {
    'use strict';
@@ -14,18 +18,27 @@ define([
     function init(el, context, config, mediator) {
         // DEBUG: What we get given on boot
       
-
+        //renders the shell
         var base = new AppTemplate({
             el: el,
             components: {
                 navTemplate: NavTemplate,
-                podcastTemplate: PodcastTemplate
+                podcastTemplate: PodcastTemplate,
+                playerTemplate: PlayerTemplate
             },
             data: {
                 nav: new Array(12)
             }
         })
 
+        //get the data
+        var SPREADSHEET_KEY = '19ftM9AI6WngBiZHQwKXxRcN9nFZUWx2Guq-UOuSd5yU';
+        get('http://interactive.guim.co.uk/spreadsheetdata/'+SPREADSHEET_KEY+'.json')
+            .then(JSON.parse)
+            .then(function(json){
+                console.log(json)
+                //linkmap = json.sheets.cardmapping;
+            });
 
 
         // Enable iframe resizing on the GU site
